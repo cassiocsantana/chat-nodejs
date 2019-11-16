1bf01bb118dbc0b7b917810a672297860dcc6b57
Cássio Cavalcante Santana <cassiocsantana@hotmail.com>
2019-11-16 16:53:58 -0400

    New Version

--- client.js
+++ client.js
@@ -1,44 +1,60 @@
 const net = require("net");
 const readline = require("readline");
 
-const client = new net.Socket();
+let AdressIPServer = "";
+let AdressPORTServer = "";
+let User = "";
 
-let user = "";
-let Ip = "192.168.10.118";
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
   // prompt: "You > "
 });
 
-//rl.prompt();
+// Set Quetions
 
-if (user === "" || user == null) {
-  client.connect(4000, Ip, async () => {
-    await console.log("------------Chat--------------");
-    await rl.question("Infrome seu nome Usuário:", requestion => {
-      user = requestion;
-      //client.write("User " + user + " Conectado com Sucesso...");
-      //rl.close(); //
-    });
-
-    await rl.addListener("line", line => {
-      client.write(user + ": " + line);
-    });
+function ask(questionText) {
+  return new Promise((resolve, reject) => {
+    rl.question(questionText, input => resolve(input));
   });
 }
 
-client.on("end", () => {
-  console.log("Servidor Desconectou");
-  client.close();
-});
+// Questions
 
-client.on("error", function(e) {
-  if (e.code == "EADDRINUSE") {
-    console.log("Tentando conectar, carregando...");
-    setTimeout(function() {
-      client.connect(4000, Ip);
-      //client.close();
+async function questions() {
+  AdressIPServer = await ask("INFORME O IP DE SUA MAQUINA: ");
+  AdressPORTServer = await ask("INFORME A PORTA: ");
+  User = await ask("What is User? ");
+}
+
+async function load_client() {
+  await questions();
+  await set_config_client(AdressIPServer, AdressPORTServer, User);
+}
+
+load_client();
+
+function set_config_client(IPServer, PortServer, user) {
+  console.log("------------Chat--------------");
+  const client = new net.Socket();
+  client.connect(PortServer, IPServer, () => {
+    rl.addListener("line", line => {
+      client.write(user + ": " + line);
     });
-  }
-});
+  });
+
+  client.on("end", () => {
+    console.log("Servidor Desconectou");
+    client.close();
+  });
+
+  client.on("error", function(e) {
+    if (e.code == "EADDRINUSE") {
+      console.log("Tentando conectar, carregando...");
+      setTimeout(function() {
+        client.close();
+        client.connect(PortServer, IPServer);
+      });
+    }
+  });
+}
--- server.js
+++ server.js
@@ -1,33 +1,62 @@
 const net = require("net");
+const readLine = require("readline");
 
-let IP = "192.168.10.118",
-  PORT = 4000;
+//Definição das variaveis
+
+let AdressIPServer = "";
+let AdressPORTServer = "";
+
+//Criação da Interface de leitura
+
+const rl = readLine.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+// set param socket
 
 const handleConnetion = async socket => {
   let remoteAddress = socket.remoteAddress + ":" + socket.remotePort;
   console.log("Nova conexão de:", remoteAddress);
   socket.setEncoding("utf8");
-
-  socket.on("data", data => {
-    console.log(data.toString());
-  });
-
-  socket.on("end", () => {
-    console.log(remoteAddress, "desconectou-se");
-  });
+  socket.on("data", data => console.log(data.toString()));
+  socket.on("end", () => console.log(remoteAddress, "desconectou-se"));
 };
-const server = net.createServer(handleConnetion);
-server.on("error", err => {
-  console.log("Erro ao iniciar servidor!");
-});
-server.listen(PORT, IP);
 
-server.on("error", function(e) {
-  if (e.code == "EADDRINUSE") {
-    console.log("Endereço sendo usado, carregando...");
-    setTimeout(function() {
-      server.close();
-      server.listen(PORT, IP);
-    }, PORT);
+const server = net.createServer(handleConnetion);
+server.on("error", err => console.log("Erro ao iniciar servidor!"));
+
+// Inicio Da leitura
+
+function ask(questionText) {
+  return new Promise((resolve, reject) => {
+    rl.question(questionText, input => resolve(input));
+  });
+}
+
+async function questions() {
+  AdressIPServer = await ask("INFORME O IP DE SUA MAQUINA: ");
+  AdressPORTServer = await ask("INFORME A PORTA: ");
+}
+
+async function load_server() {
+  await questions();
+  await set_server(AdressIPServer, AdressPORTServer);
+}
+
+function set_server(IPServer, PortServer) {
+  if (IPServer && PortServer) {
+    server.listen(PortServer, IPServer);
+    server.on("error", function(e) {
+      if (e.code == "EADDRINUSE") {
+        console.log("Endereço sendo usado, carregando...");
+        setTimeout(function() {
+          server.close();
+          server.listen(PortServer, IPServer);
+        }, PortServer);
+      }
+    });
   }
-});
+}
+
+load_server();
