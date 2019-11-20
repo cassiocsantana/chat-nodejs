aa9276bcdaf45392d83c25937e07f5a85632c1c4
Cássio Cavalcante Santana <cassiocsantana@hotmail.com>
2019-11-20 15:19:40 -0400

    Comentarios

--- client.js
+++ client.js
@@ -1,15 +1,14 @@
 const net = require("net");
 const readline = require("readline");
 
-let AdressIPServer = "";
-let AdressPORTServer = "";
-let User = "";
-let Flag = true;
+let AddressIpServer = ""; // armazena ip
+let AddressPortServer = ""; // armazena porta
+let User = ""; // // armazena nome do usuário
 
-const rl = readline.createInterface(process.stdin, process.stdout);
-rl.setPrompt(" You >");
+const rl = readline.createInterface(process.stdin, process.stdout); // cria interface de leitura e escrita
+rl.setPrompt(" You >"); // essa função funciona quando quer
 
-// Set Quetions
+// função organizar o codigo minizando e para receber entrada de dados
 
 function ask(questionText) {
   return new Promise((resolve, reject) => {
@@ -17,43 +16,50 @@ function ask(questionText) {
   });
 }
 
-// Questions
+// Configurações iniciar para o cliente se conectar
 
 async function questions() {
-  AdressIPServer = await ask("INFORME O IP DO SERVIDOR: ");
-  AdressPORTServer = await ask("INFORME A PORTA: ");
+  AddressIpServer = await ask("INFORME O IP DO SERVIDOR: ");
+  AddressPortServer = await ask("INFORME A PORTA: ");
   User = await ask("What is User? ");
 }
 
 async function load_client() {
   await questions();
-  await set_config_client(AdressIPServer, AdressPORTServer, User);
+  await set_config_client(AddressIpServer, AddressPortServer, User);
 }
 
 load_client();
 
+// configurações para ser enviada ao servidor
 function set_config_client(IPServer, PortServer, user) {
   const client = new net.Socket();
-  console.log("------------Chat--------------");
+  console.log("------------Chat--------------"); // charme
 
   client.connect(PortServer, IPServer, function() {
+    // conecta ao servidor
     rl.prompt();
   });
 
   client.on("data", function(data) {
+    // recebe dados do servidor
     console.log(data.toString());
   });
 
   rl.on("line", function(line) {
+    // envia os dados para o servidor
     client.write(user + ": " + line);
   });
 
   client.on("close", function() {
+    // trata evendo de quando sair do servidor
     console.log("Connection closed");
     process.exit(0);
   });
 
   client.on("error", () => {
+    // informa que o ip pode estar errado.
     console.log("Verifique o ip e porta..");
+    client.close();
   });
 }
--- server.js
+++ server.js
@@ -1,41 +1,43 @@
 const net = require("net");
 const readLine = require("readline");
 
-//Define Var
-let Clients = [];
-let AdressIPServer = "";
-let AdressPORTServer = "";
+//Define as Variaveis
+let Clients = []; // Array de clientes que irão se conectar
+let AddressIpServer = ""; // Armazena o Ip
+let AddressPortServer = ""; // Armazena a Porta
 
-// Create interface of read
+// Criação da interface de entrada e saida de dados no terminal
 
 const rl = readLine.createInterface({
   input: process.stdin,
   output: process.stdout
 });
 
-// set param socket
+// Configurações do servidor e ações quando receber um novo usuário
 
 const handleConnetion = async socket => {
-  socket.name = socket.remoteAddress + ":" + socket.remotePort;
-  // Add client of the list
-  Clients.push(socket);
-  broadcast(socket.name + " Conectou no Chat");
-  socket.setEncoding("utf8");
+  socket.name = socket.remoteAddress + ":" + socket.remotePort; // armazena os dados em socket para identificar o cliente
+  Clients.push(socket); // adiciona o cliente no array
+  broadcast(socket.name + " Conectou no Chat"); // da um broadcast para informar que um cliente novo esta conectado
+  socket.setEncoding("utf8"); //padroniza a entrada do teclado
 
-  // Handle incoming messages from clients.
+  // recebe os dados dos clientes
   socket.on("data", function(data) {
     broadcast(data.toString(), socket);
   });
+
+  // captura as conecções e as mantém - para questões de eventuais erros no socket
   server.getConnections(function(err, count) {
     console.log(socket.name + count);
   });
 
-  // Remove the client from the list when it leaves
+  // remove o cliente do array quando sai da conversa e da um broadcast informando aos demais
   socket.on("end", function() {
-    Clients.splice(Clients.indexOf(socket), 1);
+    Clients.splice(Clients.indexOf(socket), 1); // remove do array
     broadcast(socket.name + " > Deixou a conversa.\n", socket);
   });
 
+  // percorre o array de clientes e recebe os parametros que são as mensagens e o cliente. E somente envia a mensagem para o cliente que não é seu rementente
   function broadcast(message, sender) {
     Clients.forEach(function(client) {
       // Tratamento para nao enviar a mesma mensagem a quem originou
@@ -47,25 +49,27 @@ const handleConnetion = async socket => {
   }
 };
 
+// passa a as convigurações para o servidor e o cria
 const server = net.createServer(handleConnetion);
 
 // Inicio Da leitura
-
 function ask(questionText) {
   return new Promise((resolve, reject) => {
     rl.question(questionText, input => resolve(input));
   });
 }
 
+// cria um assincronismo da para a leitura na ordem apresentada dos dados
 async function questions() {
-  AdressIPServer = await ask("INFORME O IP DE SUA MAQUINA: ");
-  AdressPORTServer = await ask("INFORME A PORTA: ");
+  AddressIpServer = await ask("INFORME O IP DE SUA MAQUINA: ");
+  AddressPortServer = await ask("INFORME A PORTA: ");
 }
 
+// segunda parte das configurações do servidor para que possa receber as mensagens, passando endereço de porta e ip
 function set_server(IPServer, PortServer) {
   if (IPServer && PortServer) {
     server.listen(PortServer, IPServer);
-
+    // caso a porta esteja em uso
     server.on("error", function(e) {
       if (e.code == "EADDRINUSE") {
         console.log("Endereço sendo usado, carregando...");
@@ -78,9 +82,10 @@ function set_server(IPServer, PortServer) {
   }
 }
 
+// função assincrona para rodas as funções na ordem desejada.
 async function load_server() {
   await questions();
-  await set_server(AdressIPServer, AdressPORTServer);
+  await set_server(AddressIpServer, AddressPortServer);
 }
 
 load_server();
